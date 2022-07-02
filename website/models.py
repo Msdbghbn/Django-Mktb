13bc6eed87f2a95e6b99dde78008856bbc32664d
Msdbghbn <Masood.b2010@gmail.com>
2022-07-02 18:16:18 +0430

    completing contact model
    contact model is completed

--- website/models.py
+++ website/models.py
@@ -9,3 +9,7 @@ class contact(models.Model):
     massege=models.TextField(max_length=255)
     created_date=models.DateField(auto_now_add=True)
     updated_date=models.DateField(auto_now=True)
+    class Meta:
+        ordering = ['created_date']
+    def __str__(self):
+        return self.name
