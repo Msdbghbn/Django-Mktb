6a603b66167d44280809f806f24b610359c87556
Msdbghbn <Masood.b2010@gmail.com>
2022-07-02 16:07:24 +0430

    ORM with shell code
    creating classes in correspondence with tables via coding in shell

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from traitlets import default
 
 # Create your models here.
+class post(models.Model):
+    title=models.CharField(max_length=250) 
+    content=models.TextField()
+    counted_view = models.IntegerField(default=0)
+    status= models.BooleanField(default=False)
+    created_date=models.DateTimeField(auto_now_add=True)
+    updated_time=models.DateTimeField(auto_now=True)
+    published_date=models.DateTimeField(null=True)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,11 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class contact(models.Model):
+    name=models.CharField(max_length=255)
+    email=models.EmailField(max_length=255)
+    subject=models.CharField(max_length=255)
+    massege=models.TextField(max_length=255)
+    created_date=models.DateField(auto_now_add=True)
+    updated_date=models.DateField(auto_now=True)
