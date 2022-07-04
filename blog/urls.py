c1dbe5aef0d8261fbabebbdfd8f1b9b3c542ef26
Msdbghbn <Masood.b2010@gmail.com>
2022-07-04 13:15:45 +0430

    showing db data on blog
    extract and showing of data on blog

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,9 @@ from blog.views import *
 app_name = 'blog'
 urlpatterns = [ path('',blog_view,name='blog_index'),
 
-                path('single',blog_single,name='single')
+                path('single',blog_single,name='single'),
+                path('post-<int:pid>',test,name='test')
+                #path('<str:name>',test,name='test')
 
 
 ]  
