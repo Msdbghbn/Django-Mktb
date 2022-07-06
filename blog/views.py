2a38744b6b56ed34f411eddda3f43ee7b2fab3d0
Msdbghbn <Masood.b2010@gmail.com>
2022-07-06 11:47:05 +0430

    HW 6-1
    hw 6-1 is uploaded. Main changes are comparing published date with now time zone & adding counted view to each post.

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import post
-
+from django.utils import timezone
 
 def blog_view(request):
     #posts = post.objects.all()
-    posts = post.objects.filter(status=1)
+    #posts = post.objects.filter(status=1)
+    posts=post.objects.filter(published_date__lte=timezone.now())
     context={'posts':posts}
     return render(request,'blog/blog-home.html',context)
 def blog_single(request):
@@ -13,7 +14,10 @@ def blog_single(request):
 def test(request,pid):
     #posts = post.objects.get(id=pid)
     posts=get_object_or_404(post,id=pid)
+    posts.counted_view=posts.counted_view+1
+    posts.save()
     #posts = post.objects.all()
     #posts = post.objects.filter(status=0)
-    context={'post':posts}
+    context={'posts':posts}
     return render(request,'test.html',context)
+
