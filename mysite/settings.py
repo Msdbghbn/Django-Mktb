613ba2a805e9d3349e8322ee03bf6335d2773402
Msdbghbn <Masood.b2010@gmail.com>
2022-07-10 15:37:50 +0430

    S9V2 django-extendions added
    most of the features are for debugging and finding errors  and exporting an image form models ..... which barely can be used in Windows OS

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'website.apps.WebsiteConfig',
     'blog',
-    'django.contrib.humanize'
+    'django.contrib.humanize',
+    'django-extensions'
 ]
 
 MIDDLEWARE = [
