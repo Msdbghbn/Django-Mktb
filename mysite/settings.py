88b843c83d60a6cef8e57605cdb644db94411726
Msdbghbn <Masood.b2010@gmail.com>
2022-07-10 18:08:34 +0430

    S9V5  robots
    robots framework added

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,10 +42,15 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     
     'django.contrib.sites',
-    'django.contrib.sitemaps'
+    'django.contrib.sitemaps',
+    'robots',
 ]
-
+#sites framework
 SITE_ID = 2
+
+#robots frameword
+ROBOTS_USE_HOST = False
+ROBOTS_USE_SITEMAP = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
