617c8f119fab7c46a712a05d180648dec7e7a244
Msdbghbn <Masood.b2010@gmail.com>
2022-07-10 11:44:46 +0430

    S8V5 Messages modoule in contact

--- website/views.py
+++ website/views.py
@@ -3,6 +3,7 @@ from email import message
 from re import sub
 from django.shortcuts import render
 from website.forms import NameForm,ContactForm,NewsletterForm
+from django.contrib import messages
 # Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect
 
@@ -17,10 +18,12 @@ def contact_view(request):
     if request.method == 'POST':
         form=ContactForm(request.POST)
         if form.is_valid():
+
             form.save()
-            return HttpResponse('done')
+            messages.add_message(request,messages.SUCCESS,'Successfully created')
+            
         else:
-            return HttpResponse('error')
+            messages.add_message(request,messages.ERROR,' Wrong Input ')
     form=ContactForm()
     return render(request,'website2/contact.html',{'form': form})
 
