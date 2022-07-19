b25365baca544f10ae277ffdbe80a4866a506408
Msdbghbn <Masood.b2010@gmail.com>
2022-07-19 10:15:01 +0430

    S10V3 login form
    login form created

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,13 @@
 import re
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate,login
 def login_view(request):
+    username = request.GET.get('username')
+    password = request.GET.get('password')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('/')
     return render(request, 'accounts/login.html')
-
-#def logout_view(request):
-#    return 
-
 def signup_view(request):
     return render(request, 'accounts/signup.html')
