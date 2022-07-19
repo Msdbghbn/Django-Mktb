70a832b981584c141f819038379816f2e323f4e7
Msdbghbn <Masood.b2010@gmail.com>
2022-07-19 11:19:56 +0430

    S10V4  django builtin auth form

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,22 @@
 import re
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,login
+from django.contrib.auth.forms import AuthenticationForm
 def login_view(request):
-    username = request.GET.get('username')
-    password = request.GET.get('password')
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
+    if not request.user.is_authenticated:
+        form=AuthenticationForm(request=request,data=request.GET)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+        form=AuthenticationForm()
+        context={'form': form}
+        return render(request, 'accounts/login.html',context)
+    else:
         return redirect('/')
-    return render(request, 'accounts/login.html')
+
 def signup_view(request):
     return render(request, 'accounts/signup.html')
