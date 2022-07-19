18d8b1f7c281d6341a36638ca0bd8602208c13c5
Msdbghbn <Masood.b2010@gmail.com>
2022-07-19 11:55:03 +0430

    S10V5 logout func
    logout func has been added

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 import re
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.forms import AuthenticationForm
 def login_view(request):
     if not request.user.is_authenticated:
@@ -20,3 +20,8 @@ def login_view(request):
 
 def signup_view(request):
     return render(request, 'accounts/signup.html')
+
+def logout_view(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect('/')
