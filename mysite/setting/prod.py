c2c072469a0b88ca30e5584050f9f3cfab159246
Msdbghbn <Masood.b2010@gmail.com>
2022-07-20 14:19:04 +0430

    S11V1 seprate dev from prod

---
+++ mysite/setting/prod.py
@@ -0,0 +1,23 @@
+from mysite.settings import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-!51w_+6&@&*lsys+9k8-5pyu53ufd!u*wpenuhdmyc%#ome=-1'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = []
+#sites framework
+SITE_ID = 2
+
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
+]
+
+#CSRF_COOKIE_SECURE = True
--- mysite/settings.py
+++ mysite/settings.py
@@ -16,16 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!51w_+6&@&*lsys+9k8-5pyu53ufd!u*wpenuhdmyc%#ome=-1'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -51,8 +42,6 @@ INSTALLED_APPS = [
     'captcha',
     'accounts'
 ]
-#sites framework
-SITE_ID = 2
 
 #robots frameword
 ROBOTS_USE_HOST = False
@@ -99,15 +88,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
@@ -147,14 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "statics",
-]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -163,4 +141,3 @@ INTERNAL_IPS = [
     "127.0.0.1"
 ]
 
-X_FRAME_OPTIONS = "SAMEORIGIN"
